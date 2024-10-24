a8fe6d396c23f34080b27225f7010c723d584a27
chihab <chihab.mg.me@gmail.com>
2024-10-24 20:32:25 +0100

    added carousel to the hero component + improved its responsivness

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -2,12 +2,41 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
-import { Star } from "lucide-react";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { title } from "process";
+const content = [
+  {
+    title: "Power For Seamless",
+    title2: " Living",
+    subtitle: "Electricity Solutions",
+    description:
+      "Whether you're a homeowner, business owner, or community leader, we're here to light up your life with sustainable energy solutions.",
+    image: "/hero.jpg",
+  },
+  {
+    title: "Reliable Energy",
+    title2: "Solutions",
+    subtitle: "For Your Needs",
+    description:
+      "Our team of experts ensures that you have reliable and efficient energy solutions tailored to your specific requirements.",
+    image: "/hero2.png",
+  },
+  {
+    title: "Sustainable Power",
+    title2: "Solutions",
+    subtitle: "For a Better Future",
+    description:
+      "Join us in our mission to provide sustainable and eco-friendly energy solutions that benefit both you and the environment.",
+    image: "/hero3.png",
+  },
+];
 
 export default function AnimatedHero() {
   const [isClient, setIsClient] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  console.log(currentIndex);
 
   useEffect(() => {
     setIsClient(true);
@@ -16,90 +45,135 @@ export default function AnimatedHero() {
   if (!isClient) {
     return null; // or a loading placeholder
   }
+  const goToNextSlide = () => {
+    setCurrentIndex((prev) => (prev + 1) % content.length);
+  };
+  const goToPrevSlide = () => {
+    setCurrentIndex((prev) => (prev - 1 + content.length) % content.length);
+  };
 
   return (
-    <div className="relative  h-[85vh] py-10    w-full overflow-hidden">
-      <Image
-        src="/hero.jpg"
-        alt="Electrician working"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-      />
-      <div className="container mx-auto  ">
-        <div className="flex   gap-8 justify-start lg:pt-20  items-center  ">
-          <motion.div
-            initial={{ scale: 1 }}
-            animate={{ scale: 1.05 }}
-            transition={{
-              duration: 10,
-              repeat: Infinity,
-              repeatType: "reverse",
-            }}
-            className="absolute inset-0"
-          ></motion.div>
-          <div className="absolute inset-0 bg-black bg-opacity-50" />
-          <div className="relative z-10 flex h-full flex-col justify-center px-4 sm:px-6 lg:px-8">
+    <div className="relative  h-[85vh]  flex items-center     w-full overflow-hidden">
+      {content.map((item, index) =>
+        index == currentIndex ? (
+          <AnimatePresence>
             <motion.div
-              initial={{ opacity: 0, x: -50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5 }}
-              className="text-sm font-semibold uppercase tracking-wider text-gray-300"
+              className="relative h-full w-full flex items-center justify-center"
+              initial={{
+                opacity: 0.8,
+                x: 10,
+              }}
+              animate={{
+                opacity: 1,
+                x: 0,
+                transition: { duration: 0.5 },
+              }}
+              exit={{
+                opacity: 0,
+                x: -10,
+                transition: { duration: 0.5 },
+              }}
             >
-              Current Electricity Services
+              <Image
+                src={item.image}
+                alt="Electrician working"
+                layout="fill"
+                objectFit="cover"
+                quality={100}
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-50" />
+
+              <div className="flex z-50 flex-col space-y-8 absolute   right-10 top-10 sm:top-1/2 sm:-translate-y-1/2">
+                <Button
+                  onClick={goToPrevSlide}
+                  className="bg-transparent hover:bg-primary hover:text-white text-white outline-none py-6 "
+                  variant="outline"
+                >
+                  <ChevronUp />
+                </Button>
+                <Button
+                  onClick={goToNextSlide}
+                  className="bg-transparent hover:bg-primary hover:text-white text-white outline-none py-6 "
+                  variant="outline"
+                >
+                  <ChevronDown />
+                </Button>
+              </div>
+              <div className="container mx-auto     ">
+                <div className="flex  z-50 bg-lue-400 space-y-2   justify-start   items-center  ">
+                  <motion.div
+                    initial={{ scale: 1 }}
+                    animate={{ scale: 1.05 }}
+                    transition={{
+                      duration: 10,
+                      repeat: Infinity,
+                      repeatType: "reverse",
+                    }}
+                    className="absolute inset-0"
+                  ></motion.div>
+                  <div className="relative z-10 flex h-full flex-col justify-center px-4 sm:px-6 lg:px-8">
+                    <motion.div
+                      initial={{ opacity: 0, x: -50 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 0.5 }}
+                      className="text-sm font-semibold uppercase tracking-wider text-gray-300"
+                    >
+                      {item.subtitle}
+                    </motion.div>
+                    <div className="py-2   " />
+                    <motion.h1
+                      initial={{ opacity: 0, x: -50 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 0.5, delay: 0.2 }}
+                      className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl"
+                    >
+                      {item.title}
+                    </motion.h1>
+                    <motion.h1
+                      initial={{ opacity: 0, x: -50 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 0.5, delay: 0.4 }}
+                      className="text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl"
+                    >
+                      {item.title2}
+                    </motion.h1>
+                    <div className="py-2   " />
+                    <motion.p
+                      initial={{ opacity: 0, x: -50 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 0.5, delay: 0.6 }}
+                      className="mt-3 max-w-md text-base text-gray-300 sm:text-lg md:mt-5 md:max-w-3xl"
+                    >
+                      {item.description}
+                    </motion.p>
+                    <div className="py-2   " />
+                    <motion.div
+                      initial={{ opacity: 0, x: -50 }}
+                      animate={{ opacity: 1, x: 0 }}
+                      transition={{ duration: 0.5, delay: 0.8 }}
+                      className="mt-6 sm:flex sm:space-x-4"
+                    >
+                      <Button
+                        size="lg"
+                        className="px-14 w-full py-5  sm:w-auto"
+                      >
+                        Contact Us
+                      </Button>
+                      <Button
+                        variant="outline"
+                        size="lg"
+                        className="mt-3 bg-transparent hover:bg-white hover:text-primary text-white px-10 w-full sm:mt-0 sm:w-auto"
+                      >
+                        Learn More
+                      </Button>
+                    </motion.div>
+                  </div>
+                </div>
+              </div>
             </motion.div>
-              <div className="py-2   "/>
-            <motion.h1
-              initial={{ opacity: 0, x: -50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: 0.2 }}
-              className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl"
-            >
-              Power For Seamless
-            </motion.h1>
-            <motion.h1
-              initial={{ opacity: 0, x: -50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: 0.4 }}
-              className="text-4xl font-bold tracking-tight text-white sm:text-5xl lg:text-6xl"
-            >
-              Electricity Solutions
-            </motion.h1>
-              <div className="py-2   "/>
-            <motion.p
-              initial={{ opacity: 0, x: -50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: 0.6 }}
-              className="mt-3 max-w-md text-base text-gray-300 sm:text-lg md:mt-5 md:max-w-3xl"
-            >
-              Whether you&apos;re a homeowner, business owner, or community
-              leader, we&apos;re here to light up your life with sustainable
-              energy solutions.
-            </motion.p>
-              <div className="py-2   "/>
-            <motion.div
-              initial={{ opacity: 0, x: -50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.5, delay: 0.8 }}
-              className="mt-6 sm:flex sm:space-x-4"
-            >
-              <Button
-                size="lg"
-                className="px-14 py-5  sm:w-auto"
-              >
-                Contact Us
-              </Button>
-              <Button
-                variant="outline"
-                size="lg"
-                className="mt-3 bg-transparent hover:bg-white hover:text-primary text-white px-10 w-full sm:mt-0 sm:w-auto"
-              >
-                Learn More
-              </Button>
-            </motion.div>
-          </div>
-        </div>
-      </div>
+          </AnimatePresence>
+        ) : null
+      )}
     </div>
   );
 }
