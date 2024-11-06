3a2ae6a504399f54323abd1c1408def5f43ee23a
chihab <chihab.mg.me@gmail.com>
2024-11-06 20:18:06 +0100

    removed unused imports

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,8 +1,9 @@
 import { hashPassword } from "@/lib/passwords";
 import { db } from ".";
-import { productImage, products, users } from "./schema";
+// import { productImage, products, users } from "./schema";
 import * as dotenv from "dotenv";
-import slugify from "slugify";
+import { users } from "./schema";
+// import slugify from "slugify";
 dotenv.config({
   path: ".env",
 });
@@ -29,21 +30,21 @@ const registerAdminUser = async () => {
     console.log(`${email} already registered`);
   }
 };
-const productsData = [
-  {
-    name: " DEWALT 20V Max Cordless Drill/Driver Kit, 2 Batteries and Charger Included (DCD771C2) ",
-    description:
-      "    Compact, lightweight design fits into tight areas    High performance motor delivers 300 unit watts out (UWO) of power ability completing a wide range of applications    High speed transmission delivers two speeds (0-450 & 1,500 rpm) for a range of fastening and drilling applications    1/2 single sleeve ratcheting chuck provides tight bit gripping strength    Ergonomic handle delivers comfort and control    NEW 18V XR Li-Ion compact drill driver featuring XR 1.3Ah Li-Ion battery technology› See more product details ",
-    isActive: true,
-    isFeatured: true,
-    images: [
-      {
-        cloudId: "08da3132-29ac-45b8-9972-c6eeb38149c2",
-        url: "https://res.cloudinary.com/daoqriocx/image/upload/v1730795445/products/i1oezsf0teth7rrfhwga.png",
-      },
-    ],
-  },
-];
+// const productsData = [
+//   {
+//     name: " DEWALT 20V Max Cordless Drill/Driver Kit, 2 Batteries and Charger Included (DCD771C2) ",
+//     description:
+//       "    Compact, lightweight design fits into tight areas    High performance motor delivers 300 unit watts out (UWO) of power ability completing a wide range of applications    High speed transmission delivers two speeds (0-450 & 1,500 rpm) for a range of fastening and drilling applications    1/2 single sleeve ratcheting chuck provides tight bit gripping strength    Ergonomic handle delivers comfort and control    NEW 18V XR Li-Ion compact drill driver featuring XR 1.3Ah Li-Ion battery technology› See more product details ",
+//     isActive: true,
+//     isFeatured: true,
+//     images: [
+//       {
+//         cloudId: "08da3132-29ac-45b8-9972-c6eeb38149c2",
+//         url: "https://res.cloudinary.com/daoqriocx/image/upload/v1730795445/products/i1oezsf0teth7rrfhwga.png",
+//       },
+//     ],
+//   },
+// ];
 // const seedProducts = async () => {
 //   try {
 //     productsData.forEach(async (p) => {
