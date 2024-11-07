3e3ec0dfbff8836d8ddf91d8520f22ca5860c9a6
chihab <chihab.mg.me@gmail.com>
2024-11-08 00:10:01 +0100

    added the footer to the products layout

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -1,13 +1,17 @@
 import MinimalHeader from "@/components/layout/Header/MinimalHeader";
 import { ReactNode } from "react";
 import { getAllFeaturedActiveProducts } from "../data/products-data";
+import Footer from "@/components/layout/Footer";
 
 const layout = async ({ children }: { children: ReactNode }) => {
   const featuredProducts = await getAllFeaturedActiveProducts(6);
   return (
     <>
       <MinimalHeader featuredProducts={featuredProducts} />
-      <main>{children}</main>
+      <main>
+        {children}
+        <Footer />
+      </main>
     </>
   );
 };
